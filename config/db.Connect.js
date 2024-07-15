1669961abde1d5377e59d2c81411e11ef7dc2b84
Chirag Agarwal <agarwalchirag999@gmail.com>
2024-07-16 00:32:14 +0530

    db update

--- config/db.Connect.js
+++ config/db.Connect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const dbConnect = () => {
-    mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(process.env.MONGODB_URL)
         .then(() => console.log('MongoDB connected'))
         .catch(err => {
             console.error('MongoDB connection error:', err);
