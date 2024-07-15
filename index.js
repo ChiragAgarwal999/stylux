f7e1edd8455cec07ba9a99f397169ab226e5c93c
Chirag Agarwal <agarwalchirag999@gmail.com>
2024-07-15 22:58:17 +0530

    cors update

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ dbConnect();
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: 'http://127.0.0.1:5173', credentials: true }));
+// app.use(cors({ origin: 'http://127.0.0.1:5173', credentials: true }));
+app.use(cors({ origin: ['http://127.0.0.1:5173', 'https://stylux-gycx.onrender.com/'], credentials: true }));
+
 // app.use(cors());
 app.use('/public', express.static(path.join(__dirname, 'public')));
 // app.use(cookieParser());
